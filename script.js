5ed23d669998b05f0c0bf2ba242abf038e559d04
chompypotato <fletcher.hoch@outlook.com>
2025-04-08 16:41:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,5 +6,22 @@ window.addEventListener("load", () => {
     loading.style.opacity = 0;
     loading.style.visibility = "hidden";
     desktop.style.display = "flex";
-  }, 3000); // adjust this duration to your liking
+  }, 2000);
+
+  updateTime();
+  setInterval(updateTime, 1000);
 });
+
+function updateTime() {
+  const now = new Date();
+  const timeStr = now.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+  document.getElementById("time").textContent = timeStr;
+}
+
+function launchApp(name) {
+  document.getElementById(`app-${name}`).style.display = "flex";
+}
+
+function closeApp(name) {
+  document.getElementById(`app-${name}`).style.display = "none";
+}
